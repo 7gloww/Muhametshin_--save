19ba97a65d0474bf0df74d3f78dadeb565cdd3c5
prodUpal <tim.ism06@gmail.com>
2026-02-20 12:05:09 +0500

    Добавьте файлы проекта.

---
+++ AgentPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Muhametshin_Глазки_save
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TBoxSearch_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            if (TBoxSearch.Text == "Введите для поиска")
+            {
+                TBoxSearch.Text = "";
+                TBoxSearch.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TBoxSearch_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TBoxSearch.Text))
+            {
+                TBoxSearch.Foreground = Brushes.Gray;
+                TBoxSearch.Text = "Введите для поиска";
+            }
+        }
+    }
+}
